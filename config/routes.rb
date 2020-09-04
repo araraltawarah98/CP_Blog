f9aa6ec1a395913771f9f80ffcb4af08ea055578
arar <arar.altawarah98@gmail.com>
2020-09-04 19:38:17 +0300

    configured comment routes (create action)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret' }
   resources :blogs
 
+  get 'comments/:id', to: 'comment#create', as: 'comments'
   get 'profile/:id', to: 'profile#index', as: 'profile_user'
   root to: 'blogs#index'
 end
